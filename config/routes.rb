7a86a5f326344cc7e9e4e6ea4fff65bc810fef3d
Miguel Silva <msilva0897@gmail.com>
2017-11-27 21:40:26 -0700

    added controllers and endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show] do
+        collection do
+          get '/find_all', to: 'merchants/search#index'
+          get '/find', to: 'merchants/search#show'
+          get '/random', to: 'merchants/random#show'
+        end
+      end
+    end
+  end
 end
