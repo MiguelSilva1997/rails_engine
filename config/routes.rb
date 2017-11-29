74a77c4a66886002e64f9d0a3df3792b83a6356e
Miguel Silva <msilva0897@gmail.com>
2017-11-28 19:18:46 -0700

    routes are done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,62 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :transactions do
+        get '/find_all', to: 'search#index'
+        get '/find', to: 'search#show'
+      end
+
+      resources :transactions, only: [:index, :show]do
+         resources :invoice, only: [:index], to: 'transactions/invoice#index'
+      end
+
+      namespace :invoice_items do
+        get '/find_all', to: 'search#index'
+        get '/find', to: 'search#show'
+      end
+
+      resources :invoice_items, only: [:index, :show] do
+        resources :invoice, only: [:index], to: 'invoice_items/invoice#index'
+        resources :item, only: [:index], to: 'invoice_items/item#index'
+      end
+
+      namespace :invoices do
+        get '/find_all', to: 'search#index'
+        get '/find', to: 'search#show'
+      end
+
+      resources :invoices, only: [:show, :index] do
+        resources :transactions, only: [:index], to: 'invoices/transactions#index'
+        resources :merchant, only: [:index], to: 'invoices/merchant#index'
+        resources :items, only: [:index], to: 'invoices/items#index'
+        resources :customer, only: [:index], to: 'invoices/customer#index'
+        resources :invoice_items, only: [:index], to: 'invoices/invoice_items#index'
+      end
+
+      namespace :customers do
+        get '/find_all', to: 'search#index'
+        get '/find', to: 'search#show'
+      end
+
+      resources :customers, only: [:index, :show] do
+        resources :invoices, only: [:index], to: 'customers/invoices#index'
+        resources :transactions, only: [:index], to: 'customers/transactions#index'
+        resources :favorite_merchant, only: [:index]
+      end
+
+      namespace :items do
+        resources :most_items, only: [:index]
+        resources :most_revenue, only: [:index]
+        get '/find_all', to: 'search#index'
+        get '/find', to: 'search#show'
+      end
+
+      resources :items, only: [:show, :index] do
+        resources :best_day, only: [:index]
+        resources :invoice_items, only: [:index], to: 'items/invoice_items#index'
+        resources :merchant, only: [:index], to: 'items/merchant#index'
+      end
+
       namespace :merchants do
         resources :most_revenue, only: [:index]
         resources :revenue, only: [:index]
